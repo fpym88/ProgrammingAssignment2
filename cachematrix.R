82a58488d9e59f7da2b95a3e586d40d838b488ec
fpym88 <fletcherpym@gmail.com>
2019-12-05 19:59:58 +0800

    Assignment 2 submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A set of functions that look at caching and retirinvg inverse matricies
+## 
 
-## Write a short comment describing this function
+## The first function creates a matrix that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inv) inv <<- inv
+  getinverse <- function() inv
+  list(set = set, get= get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Function looks for inverse of matrix and reports
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("Getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
+
+## Test with a simple matrix
+
+m1 <- matrix(c(1/2,-1/3,-2/3,3/4), nrow = 2, ncol = 2)
+
+testmatrix <- makeCacheMatrix(m1)
+
+cacheSolve(testmatrix)
+
+## Second test with a simple matrix
+
+m2 <- matrix(c(5/7,-2/3,-3/8,5/9), nrow = 2, ncol = 2)
+
+testmatrix2 <- makeCacheMatrix(m2)
+
+cacheSolve(testmatrix2)
